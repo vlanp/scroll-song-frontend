e0b9e596c0b3af95080af7d781f14b4430fabf81
Valentin <guillaumevalentin76@gmail.com>
2025-04-30 16:58:15 +0200

    Ajout d'un bouton pour revenir en arrière

--- components/ScreenTitle.tsx
+++ components/ScreenTitle.tsx
@@ -1,5 +1,7 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import Baseline from "./Baseline";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { useRouter } from "expo-router";
 
 export default function ScreenTitle({
   title,
@@ -8,9 +10,20 @@ export default function ScreenTitle({
   title: string;
   baseline?: string | undefined;
 }) {
+  const router = useRouter();
+  const canGoBack = router.canGoBack();
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{title}</Text>
+      <View style={styles.titleView}>
+        <Text style={styles.text}>{title}</Text>
+        {canGoBack && (
+          <Pressable style={styles.iconView} onPress={() => router.back()}>
+            <MaterialIcons name="arrow-back" size={24} color="white" />
+            <Text style={styles.iconText}>Retour</Text>
+          </Pressable>
+        )}
+      </View>
       {baseline && <Baseline>{baseline}</Baseline>}
     </View>
   );
@@ -20,9 +33,28 @@ const styles = StyleSheet.create({
   container: {
     gap: 10,
   },
+  titleView: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
   text: {
     color: "white",
     fontSize: 24,
     fontFamily: "Poppins-Bold",
+  },
+  iconText: {
+    fontFamily: "Poppins-SemiBold",
+    color: "white",
+    fontSize: 16,
+  },
+  iconView: {
+    borderWidth: 2,
+    borderRadius: 10,
+    borderColor: "white",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 5,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
   },
 });
